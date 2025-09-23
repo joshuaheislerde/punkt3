c79cb57099719c4a3334ab75605787a8c8f3f4ba
Joshua Heisler <contact@extension-inspector.com>
2025-09-23 14:07:45 +0200

    feat: menu items are not sorted by default

--- app/stores/landing.store.ts
+++ app/stores/landing.store.ts
@@ -4,6 +4,15 @@ export const useLandingStore = defineStore('LandingPage', () => {
   const landingData = ref<Landing | null>(null)
 
   function setLandingData(data: Landing) {
+    if (data && data.menu_items && Array.isArray(data.menu_items)) {
+      data.menu_items.sort((a: MenuItem, b: MenuItem) => {
+        const sortA = a.sort ?? Infinity;
+        const sortB = b.sort ?? Infinity;
+
+        return Number(sortA) - Number(sortB);
+      });
+    }
+
     landingData.value = data;
   }
 
